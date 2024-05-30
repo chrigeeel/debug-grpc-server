d7d0974638a96875e4cff4c5460541667535dd02
Anonymous <>
2024-05-30 16:41:41 +0200

    initial

---
+++ client/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"time"
+
+	pb "github.com/chrigeeel/debug-grpc-server/protos"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
+)
+
+const (
+	SERVER_ENDPOINT = ""
+)
+
+func main() {
+	proxyURL, _ := url.Parse("http://HJUW814842:FHJKL027@204.52.120.1:6344") //proxy here
+
+	customDialer := func(ctx context.Context, addr string) (net.Conn, error) {
+		proxyConn, err := (&http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}).DialContext(ctx, "tcp", addr)
+		if err != nil {
+			return nil, err
+		}
+
+		return proxyConn, nil
+	}
+
+	//connection options
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})),
+		grpc.WithContextDialer(customDialer),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:                10 * time.Second,
+			Timeout:             20 * time.Second,
+			PermitWithoutStream: true,
+		}),
+	}
+
+	conn, err := grpc.DialContext(context.Background(), SERVER_ENDPOINT, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := pb.NewIPServiceClient(conn)
+
+	log.Println(client.GetIP(context.TODO(), nil))
+}
---
+++ server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/chrigeeel/debug-grpc-server/protos"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
+)
+
+type server struct {
+	pb.UnimplementedIPServiceServer
+}
+
+func (s *server) GetIP(ctx context.Context, req *pb.IPRequest) (*pb.IPResponse, error) {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("failed to get peer from context")
+	}
+
+	clientIP, _, err := net.SplitHostPort(p.Addr.String())
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse client IP: %v", err)
+	}
+
+	return &pb.IPResponse{Ip: clientIP}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterIPServiceServer(grpcServer, &server{})
+	log.Println("Server is running on port :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
